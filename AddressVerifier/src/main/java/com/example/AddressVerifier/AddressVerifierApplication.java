2f3d05272f57c839e75223568ff7c1eed9050562
hamtana <hamtana156@gmail.com>
2026-01-20 12:15:30 +1300

    added springboot project

---
+++ AddressVerifier/src/main/java/com/example/AddressVerifier/AddressVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.AddressVerifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressVerifierApplication.class, args);
+	}
+
+}
